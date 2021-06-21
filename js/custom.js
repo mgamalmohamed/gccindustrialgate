5d06c001631ddf82ca65a4ec57cb69a50a2dcc14
Mohamed Gamal <mgamalit87@gmail.com>
2021-06-21 06:49:23 +0400

    Clients Logos, Banners, Video Added

--- js/custom.js
+++ js/custom.js
@@ -690,3 +690,11 @@ close.addEventListener('click', () => {
 })
 }
 
+let counter = 1;
+setInterval(function(){
+	document.getElementById('radio' + counter).checked = true;
+	counter++;
+	if(counter > 4) {
+		counter = 1;
+	}
+}, 5000);
